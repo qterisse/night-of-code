611ca887b19b1dbb5e88e4cdcb200dfea410c5b2
Milo Niemaz <mniemaz@z1r5p4.42lyon.fr>
2025-12-05 03:32:27 +0100

    gamepage init

--- frontend/src/containers/GamePage.tsx
+++ frontend/src/containers/GamePage.tsx
@@ -1,43 +1,10 @@
-import { useEffect } from "react";
-import { useAppContext } from "../contexts/AppContext";
-import { socket } from "../services/socket";
-
-
-
 const GamePage = () => {
-
-	const { room } = useAppContext();
-
-	
-	// let totoroom = {
-	// 	players: [],
-	// 	state: "", 
-	// 	whoamiID: 1 // besoin de savoir qui je suis
-
-	// }
-
-	useEffect(() => {
-		if (!room) return;
-		console.log('my room:', room);
-		if (room?._players.length >= 4) {
-			socket.emit("start");
-		}
-	}, [room]);
-
-
-
+    
 	return (
-    <div className="relative w-screen px-30 h-screen flex flex-col items-center justify-center font-montserrat">
-    	<div className="border border-2 my-30 rounded-md flex-1 w-full items-center justify-center font-montserrat">
-				{room?._state == "waiting" && (
-					<p>
-						Waiting for players...
-					</p>
-				)}
-			</div>
-
+		<div className="relative w-screen h-screen flex flex-col items-center justify-center font-montserrat">
+			GamePage
 		</div>
-	)
+	) 
 }
 
 export default GamePage;
