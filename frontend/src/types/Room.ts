2d97350b82d377ba4dc34c8bc935c4fa7de167fc
Milo Niemaz <mniemaz@z1r5p4.42lyon.fr>
2025-12-05 01:53:18 +0100

    tryinggggg

--- frontend/src/types/Room.ts
+++ frontend/src/types/Room.ts
@@ -2,7 +2,7 @@ import type { Player } from "./Player";
 
 export type Room = {
 	_roomID: number;
-	_players: any[];
+	_players: Map<number, Player>;
 	_state: "waiting" | "in_progress" | "finished";
 	_playedCards: number[];
 };
